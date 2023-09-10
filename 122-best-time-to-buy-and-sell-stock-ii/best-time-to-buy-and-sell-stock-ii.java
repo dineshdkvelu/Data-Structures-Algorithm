06b51d46eb37450b603072a03b8c4f5e36cead95
dineshdkvelu <114653111+dineshdkvelu@users.noreply.github.com>
2023-09-10 13:35:36 +0530

    Time: 1 ms (85.77%) | Memory: 44.1 MB (82.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,27 +1,40 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        Stack<Integer> store = new Stack<>();
-        int n = prices.length;
-        int res =0;
-        int next[] = new int[n];
-        for(int j=n-1; j>=0; j--){
-            if(!store.isEmpty()){
-                if(prices[j] >= store.peek()){
-                    while(!store.isEmpty() && prices[j] >=  store.peek()) store.pop();
-                    store.push(prices[j]);
-                }
-                if(prices[j] < store.peek()){
-                    res += store.peek() - prices[j];
-                    store.push(prices[j]);
-                }
+    // public int maxProfit(int[] prices) {
+    //     Stack<Integer> store = new Stack<>();
+    //     int n = prices.length;
+    //     int res =0;
+    //     int next[] = new int[n];
+    //     for(int j=n-1; j>=0; j--){
+    //         if(!store.isEmpty()){
+    //             if(prices[j] >= store.peek()){
+    //                 while(!store.isEmpty() && prices[j] >=  store.peek()) store.pop();
+    //                 store.push(prices[j]);
+    //             }
+    //             if(prices[j] < store.peek()){
+    //                 res += store.peek() - prices[j];
+    //                 store.push(prices[j]);
+    //             }
 
-            }else store.push(prices[j]);
+    //         }else store.push(prices[j]);
+    //     }
+    //     return res;
+    // }
+     public int maxProfit(int[] prices) {
+		// We need prices for 2 days at least to find the profit.
+        if (prices == null || prices.length <= 1) {
+            return 0;
         }
-        //System.out.println(Arrays.toString(next));
-        // int res =0;
-        // for(int i=0; i<n; i++){
-        //     if(next[i] != 0) res += next[i] - prices[i];
-        // }
-        return res;
+
+        int totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // Checking if we can profit with previous day's price.
+            // If yes, then we buy on previous day and sell on current day.
+            // Add all such profits to get the total profit.
+            if (prices[i - 1] < prices[i]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return totalProfit;
     }
 }
